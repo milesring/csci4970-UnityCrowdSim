6d814030670cec3116738c6f00b0608dafee05e6
BenedictRasmussen <benedictrasmussen@gmail.com>
2017-09-13 21:16:29 -0500

    Moved many files into more organized location, renamed Navigator

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var NavMeshAgent = this.GetComponent<NavMeshAgent>();
+        NavMeshAgent.destination = new Vector3(100, 0, 8.4f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
