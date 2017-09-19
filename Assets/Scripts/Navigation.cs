70bbd254d60582731136351c14365a9d658a20ae
BenedictRasmussen <benedictrasmussen@gmail.com>
2017-09-18 22:55:01 -0500

    Created materials folder, added agent prefabs, added target to nav

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -2,11 +2,18 @@
 using UnityEngine.AI;
 
 public class Navigation : MonoBehaviour {
+    public Transform target;
 
 	// Use this for initialization
 	void Start () {
         var NavMeshAgent = this.GetComponent<NavMeshAgent>();
-        NavMeshAgent.destination = new Vector3(100, 0, 8.4f);
+        if (target != null) {
+            //Debug.Log(message: "Transform found: " + target.position);
+            NavMeshAgent.destination = target.position;
+        } else {
+            // Use default location
+            NavMeshAgent.destination = new Vector3(100, 0, 8.4f);
+        }
 	}
 	
 	// Update is called once per frame
