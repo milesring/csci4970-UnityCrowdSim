5fc3f85bd086120f06974816c15a2f77c9b04a17
Kevin Chen <chenk.dev@gmail.com>
2017-10-09 21:10:49 -0500

    added HUD prefab/script

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+	[SerializeField]
+	private Text _numberOfActors;
+
+	private readonly string ACTOR_NUM_FORMAT= "Number of Actors: {0}";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_numberOfActors.text = string.Format(ACTOR_NUM_FORMAT, GameObject.FindGameObjectsWithTag("Agent").Length);
+	}
+}
